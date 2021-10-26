798fbba67873cd0a17e19b1b29e0849f1a9be036
smail2903001 <oukhellou.fst@uhp.ac.ma>
2021-10-26 19:43:03 +0100

    Voici ma meilleur version de code conversion des bases vers la base decimale

--- Test1.c
+++ Test1.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include <math.h>
 #include<string.h>
+#include<math.h>
 unsigned int getvalue(char entree)      //fonction pour convertir le carctre en un nombre :)
 {
   switch(entree)
@@ -112,157 +112,99 @@ unsigned int getvalue(char entree)      //fonction pour convertir le carctre en 
   }
 }
 
-int longueurChaine(const char* chaine)
-{
-    int nombreDeCaracteres = 0;
-    char i;
-    char caractereActuel = 0;
 
-    do
-    {
-        caractereActuel = chaine[nombreDeCaracteres];
-        nombreDeCaracteres++;
-
+int check_string(char T[],int n,int B) {
+int check1=0, check2=0,i,D;
+for(i=0;i<n;i++) { // incrementation de compteur au cas ou on tape qlqchose bizzare
+    D=getvalue(T[i]);
+    if(D>=B && D<40) { // des caracteres alphanumeriques mais hors de base origine
+        check1++;
     }
-    while(caractereActuel != '\0'); // On boucle tant qu'on n'est pas arrivé à l'\0
-
-    //if (caractereActuel=' ' )nombreDeCaracteres++;
-
-
-
-
-    nombreDeCaracteres--; // On retire 1 caractère de long pour ne pas compter le caractère \0
-
-    return nombreDeCaracteres;
+    else if (D==40) { // cas ou on saisit des caracteres non alphanumeriques comme (,.'=" ...
+        check2++;
+    }
 }
 
+
+//Descussion des cas possible d'entree
+//1ere cas ===> hors de base
+if(check1!=0 && check2==0) {
+    return 1;
+}
+
+// 2eme cas ===> caractere non alphanumeriques
+else if ( check2!=0) {
+    return 2;
+}
+
+// 3eme cas ===> caractere acceptable ( dans la base origine )
+else if ( check1==0 && check2==0) {
+    return 3;
+}
+
+
+}
+
+
 int main() {
+    int n ,B ,v,i,j;
+    char T[250];
+    printf("BONJOUR :) ");
+    printf("Saisir la base d'origine ==>  ");
+    scanf("%d",&B);
+    printf("Veuillez vous svp entrer le numero a convertir  :   ");
+    scanf("%s",&T);
+    n=strlen(T);
+    v=check_string(T,n,B);
+    printf("===> %d", v);
 
 
-
-    int a,b,i,j,res,longueur = 0;
-    char y;
-    printf("Combien de chiffre dans le numero a convertir ");
-    scanf("%d",&a);
-    int t[a];
-    char T[a];
-    printf("Quelle est la base d'origine[2] et [35] ?  ==> ");
-    scanf("%d",&b);
-    while(b<2 || b>36 ){
-        printf(":(  Ce nes't pas une base possible !!!! \n");
-        printf(" choisissez vous autre fois ====> ");
-        scanf("%d",&b);
-    }
-    system("cls");// fonction qui efface le console
-
-
-
-
-
-
-
-    //juste une decoration de l'affichage
-    //================================================
-    for(i = 0; i < 50; i++) putchar('#');
-    printf("\n");
-    printf("Veuillez vous saisir le numero a convertir \n");
-    for(i = 0; i < 50; i++) putchar('#');
-    printf("\n");
-
-    //================================================
-
-
-
-
-
-
-
-
-    for(i=0;i<a;i++) {
-      system("cls");
-      getchar();
-      printf("Tape le chiffre d'ordre %d  : ",i+1);
-      scanf("%c",&y);
-//      longueur = longueurChaine(y);
-//      while (longueur>1){
-//        printf(":(   :(  Svp , Tape seulement les caracteres d'un seul chiffre : ");
-//        scanf("%c",&y);
-//      }
-      T[i]=y;
-      t[i]=getvalue(y);
-      while(t[i]>=b) {
-
-
-            if(t[i]>=b && t[i]<40){
+while(v==1 || v==2) {
+    if(v==1) {
             system("cls");
             printf("!!!!!! ATTENTION [ERREUR COMMIS]\n");
             printf("le chiffre que vous essayez de taper est hors de la base origine \n");
             printf("---------------------------------------------------------------------\n");
             printf("---------------------------------------------------------------------\n");
-            printf("Retapez le chiffre d'ordre %d ===>  ",i+1);
-            getchar();
-            scanf("%c",&y);
-//                  longueur = longueurChaine(y);
-//            while (longueur>1){
-//            printf(":(   :(  Svp , Tape seulement les caracteres d'un seul chiffre : ");
-//            scanf("%c",&y);
-//            }
-            T[i]=y;
-            t[i]=getvalue(y);}
-
-            else if(t[i]==40){
+            printf("Veuillez vous svp retaper seulement les caracters dans la base ==>  %d\n",B);
+            printf("Saisir le numero que voulez vous convertir autre fois :  ");
+            scanf("%s",&T);
+            n=strlen(T);
+            v=check_string(T,n,B);
+    }
+    if(v==2) {
             system("cls");
             printf("!!!!!! ATTENTION [ERREUR COMMIS]\n");
             printf("la valeur erronee , svp tape seulment les caractere alpha numerique\n Exemple: abC1e5\n");
-
             printf("---------------------------------------------------------------------\n");
             printf("---------------------------------------------------------------------\n");
-            printf("Retapez le chiffre d'ordre %d ===>  ",i+1);
-//            getchar();
-            scanf("%c",&y);
-            T[i]=y;
-            t[i]=getvalue(y);
-            }
+            printf("Veuillez vous svp retaper seulement les caracters Alphanumeriques dans la base ==>  %d\n",B);
+            printf("Saisir le numero que voulez vous convertir autre fois :  ");
+            scanf("%s",&T);
+            n=strlen(T);
+            v=check_string(T,n,B);
 
-                        }
-
-        }
-
-
-
-
-        system("cls");
-        int D=0;
-        for(i=0;i<a;i++){
-            res=1;
-            // Cette Boucle a pour l'objectif de calculer power(base,longeurTableau t - (i+1) )
-            for(j=a-(i+1);j>0;j--){
-                res*=b;
-            }
-            D = D + t[i]*res;
-        }
-
-
-
-
-//================================================
-
-
-        for(int i = 0; i < 50; i++) putchar('=');
-        printf("\n");
-        for(int i = 0; i < 50; i++) putchar('=');
-        printf("\n");
-
-//================================================
-
-
-
-
-        printf("la conversion de ce nombre [[ ");
-        for(i=0;i<a;i++) {
-            printf("%c",T[i]);
-        }
-        printf(" ]] en decimale est ==> %d\n",D);
-
+    }
 }
 
+// ICI NOUS AVONS NOTRE 1ERE OPERATIONS , C'EST LA CONVERSION VERS LA BASE DECIMAL
+        system("cls");
+        int Decimal=0;
+        for(i=0;i<n;i++){
+            int res=1;
+            // Cette Boucle a pour l'objectif de calculer power(base,longeurTableau t - (i+1) )
+            for(j=n-(i+1);j>0;j--){
+                res*=B;
+            }
+           Decimal = Decimal + getvalue(T[i])*res; // decimal <== c'est le resultat
+        }
+
+
+// CETTE INSTUCTIONS POUR L'AFFICHAGE LE RESULTAT DE 1ERE CONVERSION ( BASE D'ORIGINE vers LA BASE DECIMALE)
+
+        printf("la conversion de ce nombre [[ ");
+        for(i=0;i<n;i++) {
+            printf("%c",T[i]);
+        }
+        printf(" ]] en decimale est ==> %d\n",Decimal);
+}
